eeb0547937b3e9e1fc1b99133b8f08b0e9b34150
User <User@10.11.183.5>
2014-11-18 14:25:59 +0400

    Описание коммита 2

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, Student!");
     }
     
 }
