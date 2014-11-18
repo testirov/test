c4d38e199d5d5cd9e14a2fa7ae2a78ee8b844f78
User <User@10.11.183.5>
2014-11-18 14:28:10 +0400

    Описание коммита 3

--- JavaApplication1/src/javaapplication1/NewClass.java
+++ JavaApplication1/src/javaapplication1/NewClass.java
@@ -9,14 +9,6 @@ package javaapplication1;
  *
  * @author User
  */
-public class JavaApplication1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        System.out.println("Hello, Student!");
-    }
+public class NewClass {
     
 }
